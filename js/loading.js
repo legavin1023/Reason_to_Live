dbcebe099ed71fbe70b8047c3bc608c3e7a37dae
김희선 <legavin1023@gmail.com>
2017-10-31 01:14:06 +0900

    16

---
+++ js/loading.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){   
+        $("div#loader").css("display","none");   
+});
+
+// $(window).ready(function(){ 
+//     var _showPage = function(){
+//         var loader = $("div#loader");
+//         var container = $("div#myContainer");
+//         loader.css("display","none");
+//         container.css("display","block"); 
+//     };
+// });
